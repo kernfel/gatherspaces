ff6d4fcea1a50f2057246689ab933168a70ea465
Felix B. Kern <kernfel+github@gmail.com>
2021-07-06 09:29:00 +0900

    Initial files

---
+++ ircn_StH_fg.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="ircn_StH_fg" tilewidth="32" tileheight="32" tilecount="1674" columns="54">
+ <image source="ircn_StH2_fg.png" trans="ffffff" width="1728" height="992"/>
+</tileset>
